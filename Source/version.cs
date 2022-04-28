ed098cf5ac98b2114ef09e9ff7e92b0972a4aeed
jakenater99 <jfree481@gmail.com>
2022-04-28 15:00:55 +1000

    Update version.cs
    v40428

--- Source/version.cs
+++ Source/version.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 // Version 3.5.0, year 2018+4 month 3 day 14 
-[assembly: AssemblyVersion("1.0.2")]
-[assembly: AssemblyFileVersion("1.0.2")]
+[assembly: AssemblyVersion("1.0.40428")]
+[assembly: AssemblyFileVersion("1.0.40428")]
 //
