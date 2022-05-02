16f100500c3fa0ad6093e220ec4d2a147870472a
jakenater99 <jfree481@gmail.com>
2022-05-02 12:11:24 +1000

    Update version.cs

--- Source/version.cs
+++ Source/version.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 // Version 3.5.0, year 2018+4 month 3 day 14 
-[assembly: AssemblyVersion("1.0.0.40501")]
-[assembly: AssemblyFileVersion("1.0.0.40428")]
+[assembly: AssemblyVersion("1.0.0.40502")]
+[assembly: AssemblyFileVersion("1.0.0.40502")]
 //
