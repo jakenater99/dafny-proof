f6e821128dc51d25a751a278f2b901fe0ecfe732
Jacob Freeman <jfree481@gmail.com>
2022-08-10 16:45:09 +1000

    Update version

--- Source/version.cs
+++ Source/version.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 // When changing this, also be sure to change it in the following files:
 // * Source/DafnyDriver/DafnyDriver.csproj
 // * Source/Dafny/DafnyPipeline.csproj
-[assembly: AssemblyVersion("3.8.0.40729")]
-[assembly: AssemblyFileVersion("3.8.0.40729")]
+[assembly: AssemblyVersion("0.0.1.40810")]
+[assembly: AssemblyFileVersion("0.0.1.40810")]
